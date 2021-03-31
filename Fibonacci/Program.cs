e5a358a161e0f8dd725db3619b867111b8b6d768
Martin Peralez <martin.peralez@outlook.com>
2021-03-31 09:01:03 -0500

    Console Application that uses loops to output the first 20 Fibonacci Numbers

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int currentFib = 0;
+            int nextFib = 1;
+
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine(currentFib);
+                int nextNextFib = currentFib + nextFib;
+                currentFib = nextFib;
+                nextFib = nextNextFib;
+            }
+        }
+    }
+}
